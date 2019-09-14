94879f7d7e12ac1cc454a9e19a4f4699096c4feb
Teo Oliver <teo.liz.oli@gmail.com>
2019-09-14 20:01:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette {...seedColors[5]} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PALETTE</h1>
+      </div>
+    );
+  }
+}
